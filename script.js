9d91a20fee38b4e7df803e6a45d27ea64f140e5f
ysuo_in_code <moisesmakarem@gmail.com>
2024-02-23 16:16:31 -0400

    finished

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function buttonClick(value){
 
 function handleSymbol(symbol){
     switch(symbol){
-        case 'C;
+        case 'C':
             buffer = '0';
             runningTotal = 0;
             break;
@@ -32,12 +32,58 @@ function handleSymbol(symbol){
             if(buffer.length ===1){
                 buffer = '0';
             }else{
-                buffer = buffer.toString(0, buffer.length - 1);
+                buffer = buffer.substring(0, buffer.length - 1);
             }
             break;
-        case '+':
-        case '-':
+        case '+': 
+        case '−':
         case '×':
-
+        case '÷':
+            handleMath(symbol);
+            break;
     }
 }
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer
+    }else{
+        flushOperation(intBuffer);
+    }
+    previuosOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer){
+    if(previuosOperator === '+'){
+        runningTotal+= intBuffer;
+    }else if(previuosOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previuosOperator === '×'){
+        runningTotal *= intBuffer
+    }else if( previuosOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString){
+    if(buffer === '0'){
+        buffer = numberString;
+    }else{
+        buffer += numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.calc-buttons').addEventListener('click', function(event){
+        buttonClick(event.target.innerText);
+    })
+}
+
+init();
